d1eeb7d83008a829a8321e520afd7b7d20bc5bb1
scriptmonster <ismailtaha@gmail.com>
2013-01-18 12:52:44 +0200

    example code which collects feautres with the developed code is added

---
+++ project/features/example.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# -*- coding : utf-8 -*- 
+
+from load import LoadDataFile
+from character import CollectCharBasedFeatures
+from word import CollectWordBaseFeatures
+
+import re
+
+def parseLine(line, pattern):
+    matchResult = pattern.match(line)
+    if matchResult is None:
+        return None
+    else:
+        return matchResult.group(1), matchResult.group(2)
+
+def main():
+    ldf = LoadDataFile('../../input/SMSSpamCollection')
+    ccbf = CollectCharBasedFeatures()
+    cwbf = CollectWordBaseFeatures()
+
+    linePattern = re.compile('^(?P<class>spam|ham)\\t(?P<message>.*)$')
+
+    txt = ldf.readLine()
+    while txt:
+        klass, message = parseLine(txt, linePattern)
+        print klass, '$', message, '$', ccbf.getFeatures(message), cwbf.getFeatures(message)
+        txt = ldf.readLine()
+
+
+if __name__ == '__main__':
+    main()
